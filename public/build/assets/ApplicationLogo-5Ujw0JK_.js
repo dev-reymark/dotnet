3ff7a46844ad902dcbe22da873610279af07b23f
Dsc Smart <smartdsc21@gmail.com>
2024-06-08 15:58:47 +0800

    delete 27 files and update 1 file

---
+++ public/build/assets/ApplicationLogo-5Ujw0JK_.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-BpLnvaSy.js";import{i as t}from"./chunk-UWUNWEEL-BLESMKiy.js";function r(i){return o.jsx(t,{width:50,alt:"Datalogic logo image",src:"/assets/img/logo.png"})}export{r as A};
--- public/build/assets/ApplicationLogo-yIa7JA3a.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-ZdDOTyNG.js";import{i as t}from"./chunk-UWUNWEEL-CrGY_7-w.js";function r(i){return o.jsx(t,{width:50,alt:"Datalogic logo image",src:"/assets/img/logo.png"})}export{r as A};
